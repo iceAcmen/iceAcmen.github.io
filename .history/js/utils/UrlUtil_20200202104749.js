b83a5456d68d5479a50b67aad255f780987bf5dd
shibingbing <shibingbing@midea.com>
2020-02-03 21:44:40 +0800

    update

--- .history/js/utils/UrlUtil_20200202104749.js
+++
@@ -1,8 +0,0 @@
-function getUrlKey(name,url){
-　　return decodeURIComponent((new RegExp('[?|&]' + name + '=' + '([^&;]+?)(&|#|;|$)').exec(url) || [, ""])[1].replace(/\+/g, '%20')) || null
-
-}
-
-export {
-    getUrlKey
-}
--- .history/js/utils/UrlUtil_20200202112027.js
+++
@@ -1,8 +0,0 @@
-var Urlutil = (function(){
-    return {
-        getUrlKey : function(name,url){
-        　　return decodeURIComponent((new RegExp('[?|&]' + name + '=' + '([^&;]+?)(&|#|;|$)').exec(url) || [, ""])[1].replace(/\+/g, '%20')) || null
-        
-        }
-    }
-})();
