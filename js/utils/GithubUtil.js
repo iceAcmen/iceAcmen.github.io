bab54bf487c1c9b853c6482f13c442ff25034d2f
shibingbing <shibingbing@midea.com>
2020-02-03 21:24:53 +0800

    update

--- js/utils/GithubUtil.js
+++ js/utils/GithubUtil.js
@@ -31,21 +31,17 @@ var GithubUtil = (function() {
               // https://github.com/login/oauth/authorize?client_id=aa095e4edcac79b02a6e
               let authCodeUrl = `https://github.com/login/oauth/authorize?client_id=${clientId}`;
               window.open(authCodeUrl);
-              // fetch(authCodeUrl, {
-              //   method: 'GET',
-              // }).then(res=>{
-              //   console.log('获取code响应',res);
-              // })
+              return;
             }
             console.log("code : " + code)
             let getTokenUrl = `https://github.com/login/oauth/access_token?client_id=${clientId}&client_secret=${clientSecret}&code=${code}`;
             fetch(getTokenUrl, {
               method: 'GET',
+              mode: 'no-cors',
               headers:{
                 'Accept': 'application/json'
               }
             }).then(res=>{
-              debugger
               if (!res.ok) {
                 res.text().then(json=>{
                   console.log("获取token失败", json)  
@@ -54,10 +50,10 @@ var GithubUtil = (function() {
               }
               console.log("获取token成功", res)
             }).catch(err=>{
-              debugger
               console.log("获取token失败", err)
             })
     }
+
     return {
         getUserInfo, authorization
     }
